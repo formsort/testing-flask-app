4b4c4c512dd9732d73878685097ab77d96de8ec0
Peter Lada <github@peterlada.com>
2020-05-19 00:34:42 -0400

    use function for origins, use other domain every 10 seconds

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,27 @@ import re
 import struct
 import zlib
 from binascii import unhexlify
+import logging
+import datetime
 
 from flask import Flask, current_app, Response
 from flask_cors import CORS
 
 
+logger = logging.getLogger("origins")
+
+
+def o():
+    now = datetime.datetime.now()
+    if now.second % 10 < 5:
+        logger.warn("origins b")
+        return [re.compile(r"http://b\.formsort\.local:[0-9]+$")]
+    logger.warn("origins a")
+    return [re.compile(r"http://a\.formsort\.local:[0-9]+$")]
+
+
 a = Flask(__name__)
-CORS(a, origins=[re.compile(r"http://b\.formsort\.local:[0-9]+$")])
+CORS(a, origins=o)
 
 
 @a.before_first_request
@@ -26,21 +40,17 @@ def index():
   <body>
     <p>Loaded from A</p>
     <script>
-
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("outcome").innerHTML = "success";
-    }
-  };
-  xhttp.open("GET", "http://b.formsort.local:5000/i.png", true);
-  xhttp.send();
-}
-
-        loadDoc();
+    fetch('http://b.formsort.local:5000/inner')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .catch(error => document.getElementById("outcome").innerHTML = error)
+        .then(txt => document.getElementById("outcome").innerHTML = txt);
     </script>
-    <p>Loading image from B. Outcome: <b id="outcome">FAIL</b></p>
+    <p>Loading image from B. Outcome: <b id="outcome"></b></p>
   </body>
 </html>"""
 
@@ -62,3 +72,8 @@ PNG = (
 @a.route("/i.png")
 def img():
     return Response(PNG, mimetype="image/png")
+
+
+@a.route("/inner")
+def inner():
+    return Response("<b>success</b>", mimetype="text/html")
